fbee8f6b7b650481943be6c94cd1ac81a6309763
Jonathan Dunlap <jonnyd2885@gmail.com>
2019-08-19 22:24:37 +0700

    Initial Commit

---
+++ Personal_Project/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Personal_Project
+{
+    public class Login
+    {
+        public Login()
+        {
+
+            //Capture users login information
+            //Create login array
+            //Create password array
+
+            string[] userName;
+            string[] passWord;
+
+        }
+    }
+}
---
+++ Personal_Project/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Personal_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a login option
+            /*
+             *  ask if they login, register, or continue without login/register
+             *
+             *  login
+             *      check user database
+             *      password
+             *
+             *  register
+             *      create username
+             *      password
+             *      enter again
+             *
+             */
+
+            //Shopping list
+            /*
+             *
+             * list of items to add to cart
+             *
+             * press number for more information or to order
+             *      provide product information
+             *      ask quantity
+             *      add or cancel
+             *
+             * chekout or continue
+             *      there are X items in your cart would you like to:
+             *      checkout: go to cart
+             *      continue: back to list
+             *
+             */
+             
+            //Checkout
+            /*
+             *
+             * Verify items
+             *      use number prompts to select between items
+             *      add or delete items
+             *
+             * Confirm your items when complete
+             *
+             * Enter card number
+             *      X out all but last 4
+             *
+             * Shipping info
+             *      verify
+             *
+             * Complete checkout
+             *
+             */
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
